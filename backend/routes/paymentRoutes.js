70261d1f96d7f6f4d7943a0235da24bebb6cad44
sasiri69 <valorantanigar@gmail.com>
2026-05-01 21:52:44 +0530

    Delete backend directory

--- backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/create-intent').post(protect, createPaymentIntent);
-
-module.exports = router;
